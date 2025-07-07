da23795449f7470acde271805ab093664f1df179
Kieran Mahon <code@designchief.ie>
2025-07-07 13:01:53 +0100

    Update to docs build folder

--- src/assets/js/lib/custom.js
+++
@@ -1,5 +0,0 @@
-Foundation.Dropdown.defaults.vOffset = 0;
-Foundation.Dropdown.defaults.hOffset = 0;
-
-Foundation.Accordion.defaults.multiExpand = true;
-Foundation.Accordion.defaults.allowAllClosed = true;
